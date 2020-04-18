4960818a605594862f1fcf08d01ee4327c42a4c7
Anar Chalk <annachak9@gmail.com>
2020-04-18 02:36:03 -0400

    added cors, helmet and removed unused codes and console.log statements

---
+++ server.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIEDEX = require("./moviedex.json");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors())
+app.use(helmet());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", function handleMovies(req, res) {
+  let results = MOVIEDEX;
+  let { genre, country, avg_vote } = req.query;
+
+
+  if (genre) {
+    results = results.filter(film =>
+      film.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+
+  if (country) {
+    results = results.filter(film =>
+      film.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+
+  if (avg_vote) {
+      results = results.filter(film => Number(film.avg_vote)>= Number(avg_vote))
+  }
+
+  res.json(results);
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
